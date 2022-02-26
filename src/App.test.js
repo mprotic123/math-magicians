c0293cb7c587d5cb7fbfbf3f013adbb2c38fbfc9
mprotic123 <milan.protic@ts9maj.edu.rs>
2022-02-26 18:15:53 +0100

    Develop tests for JS functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from '@testing-library/user-event';
+import App from './App.js';
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('nav links correct path', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('clicking on Home brings you to Home', () => {
+    const homeLink = screen.getByText('Home');
+
+    userEvent.click(homeLink);
+    const homeText = screen.getByText('Welcome to this webApp!');
+
+    expect(homeText).toBeVisible();
+  });
+  test('clicking on Quote brings you to Quote page', () => {
+    const quoteLink = screen.getByText('Quote');
+
+    userEvent.click(quoteLink);
+    const quoteName = screen.getByText('The Quote');
+
+    expect(quoteName).toBeVisible();
+  });
 });
